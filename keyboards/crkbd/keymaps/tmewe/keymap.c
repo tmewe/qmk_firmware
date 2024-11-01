ede12fdf5aa8d065b39e5989c82af6fdccc0ba76
Tim Mewe <tim.mewe@me.com>
2024-11-01 08:57:32 +0100

    feat: add layer 0 home row mods

--- keyboards/crkbd/keymaps/tmewe/keymap.c
+++ keyboards/crkbd/keymaps/tmewe/keymap.c
@@ -1,16 +1,32 @@
 #include QMK_KEYBOARD_H
 
+// Layer switches
+#define SPC_L1 MT(MO(1), KC_SPC)
+#define ENT_L1 MT(MO(1), KC_ENT)
+
+// Left-hand home row mods
+#define SFT_A LSFT_T(KC_A)
+#define CTL_S LCTL_T(KC_S)
+#define ALT_D LALT_T(KC_D)
+#define GUI_F LGUI_T(KC_F)
+
+// Right-hand home row mods
+#define GUI_J RGUI_T(KC_J)
+#define ALT_K LALT_T(KC_K)
+#define CTL_L RCTL_T(KC_L)
+#define SFT_SC RSFT_T(KC_SCLN)
+
 #ifdef LAYOUT_split_3x6_3_ex2
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [0] = LAYOUT_split_3x6_3_ex2(
   //,--------------------------------------------------------------.  ,--------------------------------------------------------------.
        KC_TAB,    KC_Q,    KC_W,    KC_E,    KC_R,    KC_T, KC_LCTL,    KC_RCTL,    KC_Y,    KC_U,    KC_I,    KC_O,   KC_P,  KC_BSPC,
   //|--------+--------+--------+--------+--------+--------+--------|  |--------+--------+--------+--------+--------+--------+--------|
-      KC_LCTL,    KC_A,    KC_S,    KC_D,    KC_F,    KC_G, KC_LALT,    KC_RALT,    KC_H,    KC_J,    KC_K,    KC_L, KC_SCLN, KC_QUOT,
+      KC_LCTL,   SFT_A,   CTL_S,   ALT_D,   GUI_F,    KC_G, KC_LALT,    KC_RALT,    KC_H,   GUI_J,   ALT_K,   CTL_L,  SFT_SC, KC_QUOT,
   //|--------+--------+--------+--------+--------+--------+--------'  `--------+--------+--------+--------+--------+--------+--------|
       KC_LSFT,    KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,                         KC_N,    KC_M, KC_COMM,  KC_DOT, KC_SLSH,  KC_ESC,
   //|--------+--------+--------+--------+--------+--------+--------.  ,--------+--------+--------+--------+--------+--------+--------|
-                                          KC_LGUI, TL_LOWR,  KC_SPC,     KC_ENT, TL_UPPR, KC_RGUI
+                                          KC_LGUI,  SPC_L1,  KC_SPC,     KC_ENT,  ENT_L1, KC_RGUI
                                       //`--------------------------'  `--------------------------'
 
   ),
